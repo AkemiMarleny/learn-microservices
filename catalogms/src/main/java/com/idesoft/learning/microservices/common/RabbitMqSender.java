653f12ee972161d46a72ab352ece6e4cd88822e6
Akemi Marleny <akemi.marleny@gmail.com>
2024-11-15 22:19:01 +0100

    chore: rabbit configuration

---
+++ catalogms/src/main/java/com/idesoft/learning/microservices/common/RabbitMqSender.java
@@ -0,0 +1,26 @@
+package com.idesoft.learning.microservices.common;
+
+import com.idesoft.learning.microservices.entities.Product;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class RabbitMqSender {
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${spring.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${spring.rabbitmq.routingkey}")
+    private String routingKey;
+
+    public void send(Product product) {
+        log.info("sending {}", product.toString());
+        rabbitTemplate.convertAndSend(exchange, routingKey, product);
+    }
+}
--- catalogms/src/main/java/com/idesoft/learning/microservices/controllers/ProductCatalogController.java
+++ catalogms/src/main/java/com/idesoft/learning/microservices/controllers/ProductCatalogController.java
@@ -1,16 +1,27 @@
 package com.idesoft.learning.microservices.controllers;
 
+import com.idesoft.learning.microservices.common.RabbitMqSender;
+import com.idesoft.learning.microservices.entities.Product;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/product-catalog")
+@RequestMapping("/api/v1/product-catalogs")
 public class ProductCatalogController {
+
+    private final RabbitMqSender rabbitMqSender;
 
     @GetMapping
     ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("{\"message\": \"hello\"}");
+
+        Product productDemo = new Product(1L, "Product Name");
+
+        rabbitMqSender.send(productDemo);
+
+        return ResponseEntity.ok("Mensaje enviado");
     }
 }
