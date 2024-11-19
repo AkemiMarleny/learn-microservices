a46a135bb69a5d09b45b20b587a350cbf4deb041
AkemiMarleny <akemi.marleny@gmail.com>
2024-11-19 22:16:57 +0100

    refactor: si ha corretto il nome al repostory (#9)

--- orderms/src/main/java/com/idesoft/learning/microservices/orderms/consumers/ProductConsumer.java
+++ orderms/src/main/java/com/idesoft/learning/microservices/orderms/consumers/ProductConsumer.java
@@ -1,8 +1,8 @@
 package com.idesoft.learning.microservices.orderms.consumers;
 
-import com.idesoft.learning.microservices.orderms.Repositories.ProductRepository;
 import com.idesoft.learning.microservices.orderms.entities.readonly.Product;
 import com.idesoft.learning.microservices.orderms.events.ProductCreatedEvent;
+import com.idesoft.learning.microservices.orderms.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
